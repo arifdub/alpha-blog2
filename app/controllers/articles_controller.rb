98d996c79ae1138bc93a3fecd43e69d1117b7b64
Arif <arifdub@gmail.com>
2016-11-15 19:55:35 +0000

    Ul article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,26 @@
 class ArticlesController < ApplicationController
     def new
+        @article= Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+        if
+            @article.save
+            flash[:notice] = "Your article was saved succefully"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    def show
+        @article = Article.find(params[:id])
     end
     def index
     end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
 end
